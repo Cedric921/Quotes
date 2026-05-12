ec3750cb58c5b8cc7b7e2f4741192811abcde6db
Cedric Karungu <ckarungu921@gmail.com>
2026-05-12 22:52:28 +0200

    build(ios): temporarily remove App Group entitlements to unblock TestFlight build

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -69,11 +69,6 @@ export default ({ config }) => {
     ios: {
       supportsTablet: true,
       bundleIdentifier: iosBundleId,
-      entitlements: {
-        "com.apple.security.application-groups": [
-          `group.${iosBundleId}.widget`,
-        ],
-      },
       infoPlist: {
         ITSAppUsesNonExemptEncryption: false,
         UIBackgroundModes: ["remote-notification"],
